e560da19b6b128fba69f66e8cba4657d01b27e4b
Sahilgupta00001 <sahil.gupta@corevaluetech.com>
2025-03-11 16:49:32 +0530

    change in app.jsx in authguard import

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import CompanySetting from "./components/CompanySetting";
 import CompanyNotifications from "./components/CompanyNotifications";
 import UserNotifications from "./components/UserNotifications";
 import Jobs from "./components/JobsApplied";
-import AuthGuard from "./utils/autogaurd";
+import AuthGuard from "./utils/authgaurd";
 
 function App() {
   const userouter = createBrowserRouter([
