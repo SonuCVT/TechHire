1b49b2eac6b63fed9101b7d0d5fc53a8adcd3449
Sahilgupta00001 <sahil.gupta@corevaluetech.com>
2025-03-17 16:22:13 +0530

    new change

--- src/components/JobOpening.jsx
+++ src/components/JobOpening.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from 'react';
-//import Header from './Header';
-import { useSelector } from 'react-redux';
-import { FaAndroid, FaPython, FaDatabase, FaFigma, FaCode, FaJava, FaBeer } from 'react-icons/fa';
-import UserDashboardHeader from './UserDashboardHeader';
-import useFetchJobs from '../hooks/useFetchJobs';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  FaAndroid,
+  FaPython,
+  FaDatabase,
+  FaFigma,
+  FaCode,
+  FaJava,
+  FaBeer,
+} from "react-icons/fa";
+
+import UserDashboardHeader from "./UserDashboardHeader";
+import useFetchJobs from "../hooks/useFetchJobs";
 const JobOpening = () => {
-  useFetchJobs()
+  useFetchJobs();
   const jobs = useSelector((state) => state.addpostjob.jobs);
   const darkMode = useSelector((state) => state.theme.darkMode);
-  
+
   const [filters, setFilters] = useState({
-    category: '',
-    location: '',
+    category: "",
+    location: "",
   });
+
+  const [activeCategory, setActiveCategory] = useState(null);
 
   const handleFilterChange = (e) => {
     setFilters({ ...filters, [e.target.name]: e.target.value });
   };
 
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+  };
+
   const handleShowAllJobs = () => {
-    setFilters({ category: '', location: '' });
+    setFilters({ category: "", location: "" });
   };
 
   const jobCategories = [
-    { name: 'Android Developer', icon: <FaAndroid /> },
-    { name: 'Python Developer', icon: <FaPython /> },
-    { name: 'Backend Developer', icon: <FaDatabase /> },
-    { name: 'Software Engineer', icon: <FaBeer /> },
-    { name: 'Full Stack Developer', icon: <FaCode /> },
-    { name: 'Java Developer', icon: <FaJava /> }
+    { name: "Android Developer", icon: <FaAndroid /> },
+    { name: "Python Developer", icon: <FaPython /> },
+    { name: "Backend Developer", icon: <FaDatabase /> },
+    { name: "Software Engineer", icon: <FaBeer /> },
+    { name: "Full Stack Developer", icon: <FaCode /> },
+    { name: "Java Developer", icon: <FaJava /> },
   ];
 
   const jobCounts = jobCategories.reduce((acc, category) => {
     acc[category.name] = jobs.filter(
-      (job) => job.title && job.title.toLowerCase().includes(category.name.toLowerCase())
+      (job) =>
+        job.title &&
+        job.title.toLowerCase().includes(category.name.toLowerCase())
     ).length;
     return acc;
   }, {});
@@ -41,27 +57,70 @@ const JobOpening = () => {
   const filteredJobs = jobs.filter((job) => {
     return (
       job.title &&
-      (!filters.category || job.title.toLowerCase().includes(filters.category.toLowerCase())) &&
-      (!filters.location || job.location?.toLowerCase().includes(filters.location.toLowerCase()))
+      (!filters.category ||
+        job.title.toLowerCase().includes(filters.category.toLowerCase())) &&
+      (!filters.location ||
+        job.location?.toLowerCase().includes(filters.location.toLowerCase()))
     );
   });
 
   return (
     <>
       <UserDashboardHeader />
-      <div className={`p-6 transition-all ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
-        <div className={`relative p-12 text-center rounded-lg ${darkMode ? 'bg-gray-800' : 'bg-blue-100'}`}>
+      <div
+        className={`transition-all ${
+          darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-black"
+        }`}
+      >
+        <div
+          className={`relative p-8 text-center rounded-lg ${
+            darkMode ? "bg-gray-800" : "bg-blue-100"
+          }`}
+        >
           <h1 className="text-4xl font-bold">Find Your Dream Job Here</h1>
           <div className="flex justify-center mt-4 space-x-2">
-            <input type="text" placeholder="Keywords" className="border p-2 rounded-md bg-transparent" />
-            <input type="text" name="location" value={filters.location} onChange={handleFilterChange} placeholder="Location" className="border p-2 rounded-md bg-transparent" />
-            <input type="text" name="category" value={filters.category} onChange={handleFilterChange} placeholder="Roles" className="border p-2 rounded-md bg-transparent" />
+            <input
+              type="text"
+              placeholder="Keywords"
+              className="border p-2 rounded-md bg-transparent"
+            />
+            <input
+              type="text"
+              name="location"
+              value={filters.location}
+              onChange={handleFilterChange}
+              placeholder="Location"
+              className="border p-2 rounded-md bg-transparent"
+            />
+            <input
+              type="text"
+              name="category"
+              value={filters.category}
+              onChange={handleFilterChange}
+              placeholder="Roles"
+              className="border p-2 rounded-md bg-transparent"
+            />
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-4 mt-6">
+        <div className="p-6 grid grid-cols-3 gap-4">
           {jobCategories.map((category) => (
-            <button key={category.name} className={`flex flex-col items-center justify-center p-6 border rounded-lg shadow-md transition duration-300 ${darkMode ? 'bg-gray-700 text-white' : 'bg-white'} ${filters.category === category.name ? 'bg-blue-500' : ''}`} onClick={() => setFilters({ ...filters, category: category.name })}>
+            <button
+              key={category.name}
+              className={`flex flex-col items-center justify-center p-6 border rounded-lg shadow-md transition duration-300 cursor-pointer hover:bg-[#48596f] hover:text-white ${
+                darkMode ? "bg-gray-700 text-white" : "bg-white"
+              } ${
+                activeCategory === category.name
+                  ? "!bg-[#48596f] text-white"
+                  : ""
+              } ${filters.category === category.name ? "bg-blue-500" : ""}`}
+              onClick={() =>
+                setFilters(
+                  { ...filters, category: category.name },
+                  handleCategoryClick(category.name)
+                )
+              }
+            >
               <div className="text-3xl">{category.icon}</div>
               <h3 className="mt-2 text-lg font-semibold">{category.name}</h3>
               <p className="text-sm">({jobCounts[category.name] || 0} jobs)</p>
@@ -69,39 +128,83 @@ const JobOpening = () => {
           ))}
         </div>
 
-        <div className="flex mt-6">
-          <div className={`w-1/4 p-4 border-r ${darkMode ? 'border-gray-600' : 'border-gray-300'}`}>
+        <div className="flex mt-3">
+          <div
+            className={`w-1/4 p-4 border-r ${
+              darkMode ? "border-gray-600" : "border-gray-300"
+            }`}
+          >
             <h2 className="font-semibold text-lg">By Category:</h2>
             {jobCategories.map((category) => (
               <label key={category.name} className="block">
-                <input type="radio" name="category" value={category.name} checked={filters.category === category.name} onChange={handleFilterChange} className="mr-2" />
+                <input
+                  type="radio"
+                  name="category"
+                  value={category.name}
+                  checked={filters.category === category.name}
+                  onChange={handleFilterChange}
+                  className="mr-2"
+                />
                 {category.name}
               </label>
             ))}
 
             <h2 className="font-semibold text-lg mt-4">By Location:</h2>
-            {['Mumbai', 'Delhi', 'Noida', 'Lucknow', 'Bangalore'].map((location) => (
-              <label key={location} className="block">
-                <input type="radio" name="location" value={location} checked={filters.location === location} onChange={handleFilterChange} className="mr-2" />
-                {location}
-              </label>
-            ))}
+            {["Mumbai", "Delhi", "Noida", "Lucknow", "Bangalore"].map(
+              (location) => (
+                <label key={location} className="block">
+                  <input
+                    type="radio"
+                    name="location"
+                    value={location}
+                    checked={filters.location === location}
+                    onChange={handleFilterChange}
+                    className="mr-2"
+                  />
+                  {location}
+                </label>
+              )
+            )}
 
-            <button onClick={handleShowAllJobs} className="mt-4 px-4 py-2 bg-red-500 text-white rounded w-full">Show All Jobs</button>
+            <button
+              onClick={handleShowAllJobs}
+              className="mt-4 px-4 py-2 bg-red-500 text-white rounded w-full cursor-pointer"
+            >
+              Show All Jobs
+            </button>
           </div>
 
-          <div className="w-3/4 p-4">
+          <div className="w-3/4 p-4 h-[40vh] overflow-y-scroll">
             {filteredJobs.length > 0 ? (
               filteredJobs.map((job, index) => (
-                <div key={index} className={`p-4 border rounded-lg shadow-md mb-4 ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
-                  <h3 className="text-xl font-semibold">{job.title}</h3>
-                  <p><strong>Company:</strong> {job.company}</p>
-                  <p><strong>Location:</strong> {job.location}</p>
-                  <p><strong>Description:</strong> {job.description}</p>
-                  <p><strong>Experience:</strong> {job.experienceLevel} years</p>
-                  <p><strong>Salary:</strong> {job.salary} CTC</p>
+                <div
+                  key={index}
+                  className={`p-4 border rounded-lg shadow-md mb-4 flex justify-between items-end ${
+                    darkMode ? "bg-gray-800" : "bg-white"
+                  }`}
+                >
+                  <div className="">
+                    <h3 className="text-xl font-semibold">{job.title}</h3>
+                    <p>
+                      <strong>Company:</strong> {job.company}
+                    </p>
+                    <p>
+                      <strong>Location:</strong> {job.location}
+                    </p>
+                    <p>
+                      <strong>Description:</strong> {job.description}
+                    </p>
+                    <p>
+                      <strong>Experience:</strong> {job.experienceLevel} years
+                    </p>
+                    <p>
+                      <strong>Salary:</strong> {job.salary} CTC
+                    </p>
+                  </div>
                   <div className="flex justify-end space-x-2 mt-2">
-                    <button className="px-4 py-2 bg-blue-500 text-white rounded">Apply</button>
+                    <button className="px-4 py-2 bg-[#48596f] text-white cursor-pointer rounded">
+                      Apply
+                    </button>
                   </div>
                 </div>
               ))
