802c6a5c886d4fa43907e4efe88e5d923621f281
Sahilgupta00001 <sahil.gupta@corevaluetech.com>
2025-03-21 12:13:48 +0530

    change in addteam member

--- src/components/AddTeamMember.jsx
+++ src/components/AddTeamMember.jsx
@@ -6,8 +6,8 @@ import { addTeamMember } from "../utils/addTeammemberSlice";
 import { useNavigate } from "react-router-dom";
 
 const AddTeamMember = () => {
-    const dispatch =useDispatch();
-    const navigate =useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,8 +21,8 @@ const AddTeamMember = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTeamMember(formData))
-    navigate("/")
+    dispatch(addTeamMember(formData));
+    navigate("/hr-dashboard");
     console.log("Member Added:", formData);
   };
 
@@ -32,7 +32,7 @@ const AddTeamMember = () => {
       <div className="flex bg-gray-100 h-[calc(100vh-64px)]">
         {/* Sidebar */}
         <Sidebar />
-       
+
         {/* Main Content */}
         <div className="flex-grow flex justify-center items-start pt-8">
           <div className="w-full max-w-2xl bg-white p-8 rounded-2xl shadow-lg">
@@ -41,7 +41,9 @@ const AddTeamMember = () => {
             </h2>
             <form onSubmit={handleSubmit} className="space-y-4">
               <div>
-                <label className="block text-gray-700 font-medium">Name :</label>
+                <label className="block text-gray-700 font-medium">
+                  Name :
+                </label>
                 <input
                   type="text"
                   name="name"
@@ -52,7 +54,9 @@ const AddTeamMember = () => {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-medium">Email :</label>
+                <label className="block text-gray-700 font-medium">
+                  Email :
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -63,7 +67,9 @@ const AddTeamMember = () => {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-medium">Mobile no. :</label>
+                <label className="block text-gray-700 font-medium">
+                  Mobile no. :
+                </label>
                 <input
                   type="text"
                   name="mobile"
@@ -74,7 +80,9 @@ const AddTeamMember = () => {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-medium">Designation :</label>
+                <label className="block text-gray-700 font-medium">
+                  Designation :
+                </label>
                 <input
                   type="text"
                   name="designation"
